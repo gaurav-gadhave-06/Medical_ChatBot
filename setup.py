d24cde113c85e73d696bc102afecf4b896874c64
gaurav-gadhave-06 <gauravgadhave152@gmail.com>
2025-03-03 10:46:07 +0530

    full project commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Gaurav Gadhave',
+    author_email= 'gauravgadhave7227@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
